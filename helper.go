b55ad07ea255252037708474e7817f787d15d0d4
a <a@tuxpa.in>
2023-02-17 01:08:10 -0600

    edit helpers

--- helper.go
+++ helper.go
@@ -2,11 +2,20 @@ package jrpc
 
 import "context"
 
-func Do[T any](ctx context.Context, c Conn, method string, args any) (T, error) {
+func Do[T any](ctx context.Context, c Conn, method string, args any) (*T, error) {
 	var t T
-	err := c.Do(ctx, t, method, args)
+	err := c.Do(ctx, &t, method, args)
 	if err != nil {
-		return t, err
+		return nil, err
 	}
-	return t, nil
+	return &t, nil
+}
+
+func Call[T any](ctx context.Context, c Conn, method string, args ...any) (*T, error) {
+	var t T
+	err := c.Call(ctx, &t, method, args)
+	if err != nil {
+		return nil, err
+	}
+	return &t, nil
 }
