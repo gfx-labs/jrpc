eb83fd68878fdaeeaa72e764be8031f98bc37771
a <a@tuxpa.in>
2023-02-28 05:32:53 -0600

    fix helper

--- helper.go
+++ helper.go
@@ -4,7 +4,7 @@ import "context"
 
 func Do[T any](ctx context.Context, c Conn, method string, args any) (*T, error) {
 	var t T
-	err := c.Do(ctx, &t, method, args)
+	err := c.Call(ctx, &t, method, args)
 	if err != nil {
 		return nil, err
 	}
