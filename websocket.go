841f541851d9402ddcf136fa6827d04d0bcd032d
a <a@tuxpa.in>
2023-06-24 14:16:21 -0500

    maybe js will work

--- websocket.go
+++ websocket.go
@@ -24,8 +24,8 @@ import (
 	"time"
 
 	"gfx.cafe/open/jrpc/wsjson"
-	"tuxpa.in/a/zlog/log"
 	"nhooyr.io/websocket"
+	"tuxpa.in/a/zlog/log"
 )
 
 const (
@@ -68,45 +68,6 @@ func (e wsHandshakeError) Error() string {
 		s += " (HTTP status " + e.status + ")"
 	}
 	return s
-}
-
-// that is listening on the given endpoint using the provided dialer.
-func DialWebsocketWithDialer(ctx context.Context, endpoint, origin string, opts *websocket.DialOptions) (*Client, error) {
-	endpoint, header, err := wsClientHeaders(endpoint, origin)
-	if err != nil {
-		return nil, err
-	}
-	opts.HTTPHeader = header
-	return newClient(ctx, func(cctx context.Context) (ServerCodec, error) {
-		conn, resp, err := websocket.Dial(cctx, endpoint, opts)
-		if err != nil {
-			hErr := wsHandshakeError{err: err}
-			if resp != nil {
-				hErr.status = resp.Status
-			}
-			return nil, hErr
-		}
-		out := newWebsocketCodec(resp.Request.Context(), conn, endpoint, header)
-		return out, err
-	})
-}
-
-// DialWebsocket creates a new RPC client that communicates with a JSON-RPC server
-// that is listening on the given endpoint.
-//
-// The context is used for the initial connection establishment. It does not
-// affect subsequent interactions with the client.
-func DialWebsocket(ctx context.Context, endpoint, origin string) (*Client, error) {
-	endpoint, header, err := wsClientHeaders(endpoint, origin)
-	if err != nil {
-		return nil, err
-	}
-	dialer := &websocket.DialOptions{
-		CompressionMode:      websocket.CompressionContextTakeover,
-		CompressionThreshold: 4096,
-		HTTPHeader:           header,
-	}
-	return DialWebsocketWithDialer(ctx, endpoint, origin, dialer)
 }
 
 func wsClientHeaders(endpoint, origin string) (string, http.Header, error) {
---
+++ websocket_nojs.go
@@ -0,0 +1,48 @@
+//go:build !js
+
+package jrpc
+
+import (
+	"context"
+
+	"nhooyr.io/websocket"
+)
+
+// that is listening on the given endpoint using the provided dialer.
+func DialWebsocketWithDialer(ctx context.Context, endpoint, origin string, opts *websocket.DialOptions) (*Client, error) {
+	endpoint, header, err := wsClientHeaders(endpoint, origin)
+	if err != nil {
+		return nil, err
+	}
+	opts.HTTPHeader = header
+	return newClient(ctx, func(cctx context.Context) (ServerCodec, error) {
+		conn, resp, err := websocket.Dial(cctx, endpoint, opts)
+		if err != nil {
+			hErr := wsHandshakeError{err: err}
+			if resp != nil {
+				hErr.status = resp.Status
+			}
+			return nil, hErr
+		}
+		out := newWebsocketCodec(resp.Request.Context(), conn, endpoint, header)
+		return out, err
+	})
+}
+
+// DialWebsocket creates a new RPC client that communicates with a JSON-RPC server
+// that is listening on the given endpoint.
+//
+// The context is used for the initial connection establishment. It does not
+// affect subsequent interactions with the client.
+func DialWebsocket(ctx context.Context, endpoint, origin string) (*Client, error) {
+	endpoint, header, err := wsClientHeaders(endpoint, origin)
+	if err != nil {
+		return nil, err
+	}
+	dialer := &websocket.DialOptions{
+		CompressionMode:      websocket.CompressionContextTakeover,
+		CompressionThreshold: 4096,
+		HTTPHeader:           header,
+	}
+	return DialWebsocketWithDialer(ctx, endpoint, origin, dialer)
+}
