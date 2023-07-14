60f97645f90c1fa3fd23265bd123ccabf0e9d15f
a <a@tuxpa.in>
2023-07-13 22:06:49 -0500

    ok

--- pkg/codec/jrpc.go
+++ pkg/codec/jrpc.go
@@ -5,6 +5,18 @@ import (
 	"io"
 )
 
+// http.handler, but for jrpc
+type Handler interface {
+	ServeRPC(w ResponseWriter, r *Request)
+}
+
+// http.HandlerFunc,but for jrpc
+type HandlerFunc func(w ResponseWriter, r *Request)
+
+func (fn HandlerFunc) ServeRPC(w ResponseWriter, r *Request) {
+	(fn)(w, r)
+}
+
 type Doer interface {
 	Do(ctx context.Context, result any, method string, params any) error
 }
