fb32194befd1b901648e5b8b981d725cfccec276
a <a@tuxpa.in>
2024-09-03 17:57:00 -0500

    noot

---
+++ contrib/middleware/timeout.go
@@ -0,0 +1,26 @@
+package middleware
+
+import (
+	"context"
+	"time"
+
+	"gfx.cafe/open/jrpc/pkg/jsonrpc"
+)
+
+func Timeout(dur time.Duration) func(h jsonrpc.Handler) jsonrpc.Handler {
+	return func(next jsonrpc.Handler) jsonrpc.Handler {
+		return jsonrpc.HandlerFunc(func(w jsonrpc.ResponseWriter, r *jsonrpc.Request) {
+			done := make(chan struct{})
+			ctx, cn := context.WithTimeout(r.Context(), dur)
+			r = r.WithContext(ctx)
+			go func() {
+				next.ServeRPC(w, r)
+				close(done)
+				cn()
+			}()
+			<-ctx.Done()
+			cn()
+			<-done
+		})
+	}
+}
